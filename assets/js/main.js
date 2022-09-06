5fd18c65213816ea05a8f2505bebde33fbcd80f4
Hossein <hosseinyaqoubib@gmail.com>
2022-09-06 21:28:46 +0430

    add special offer in owl carousel slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,6 +50,28 @@ var x = setInterval(function() {
     }
 }, 1000);
 
+$('.owl-carousel').owlCarousel({
+    rtl: true,
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: true,
+            loop: false
+        }
+    }
+})
+
 $('.tt').toolip('show');
 
 $('.po').popover('show');
